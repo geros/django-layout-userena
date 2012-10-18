282dde6ca3abf1d2ce41199689990d16c82dac74
geros <g.soulis@beetech.gr>
2012-10-18 16:46:55 +0300

    added debug from common

--- project_name/settings/development.py
+++ project_name/settings/development.py
@@ -1,5 +1,8 @@
 from common import *
 
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
